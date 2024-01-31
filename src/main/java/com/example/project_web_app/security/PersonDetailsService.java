712bf0c178e77354f602f07d3869840d6dc302e8
Ilya_Spasskiy <spasskiy.iv@yandex.ru>
2024-01-31 20:40:42 +0300

    New commit

---
+++ src/main/java/com/example/project_web_app/security/PersonDetailsService.java
@@ -0,0 +1,27 @@
+package com.example.project_web_app.security;
+
+import com.example.project_web_app.models.Person;
+import com.example.project_web_app.repo.PersonRepository;
+import com.example.project_web_app.security.PersonDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonDetailsService implements UserDetailsService {
+
+    private final PersonRepository personRepository;
+
+    @Autowired
+    public PersonDetailsService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Person person = personRepository.findByEmail(username);
+        return new PersonDetails(person);
+    }
+}
---
+++ src/main/java/com/example/project_web_app/security/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.example.project_web_app.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PersonDetailsService personDetailService;
+
+    @Autowired
+    public SecurityConfig(PersonDetailsService personDetailService) {
+        this.personDetailService = personDetailService;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers("/", "/auth", "/registration" )
+                    .permitAll()
+                .anyRequest()
+                    .authenticated()
+                .and()
+                    .formLogin()
+                    .loginPage("/auth")
+                    .loginProcessingUrl("/process_login")
+                    .defaultSuccessUrl("/", true)
+                    .failureUrl("/auth?error");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personDetailService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new  BCryptPasswordEncoder();
+    }
+}
