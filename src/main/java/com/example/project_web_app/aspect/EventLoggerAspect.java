7fe7786c512989094ad315dcff1a134d0a021d36
Ilya_Spasskiy <spasskiy.iv@yandex.ru>
2024-02-01 13:24:58 +0300

    Add slf4j to loggerAdvice

--- src/main/java/com/example/project_web_app/aspect/EventLoggerAspect.java
+++ src/main/java/com/example/project_web_app/aspect/EventLoggerAspect.java
@@ -1,17 +1,19 @@
 package com.example.project_web_app.aspect;
 
 import com.example.project_web_app.model.Event;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
 @Aspect
+@Slf4j
 public class EventLoggerAspect {
 
     @Before("com.example.project_web_app.aspect.pointcut.EventPointcut.createEventPointcut()")
     public void eventLoggerAdvice(JoinPoint joinPoint){
         Object[] args = joinPoint.getArgs();
         Event event = (Event) args[0];
-        System.out.println("Попытка создания мероприятия. Город: " + event.getTown() + " Дата: " + event.getDate());
+        log.info("Попытка создания мероприя, TOWN:{} DATE:{}", event.getTown(), event.getDate());
     }
 }
